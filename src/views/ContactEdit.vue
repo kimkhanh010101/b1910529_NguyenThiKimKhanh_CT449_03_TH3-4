7abfedd6d9db1d0df267196314ed468a967edfd0
Your Name <you@example.com>
2022-11-01 12:18:01 +0700

    Tao trang cap nhat lien he

---
+++ src/views/ContactEdit.vue
@@ -0,0 +1,72 @@
+<template>
+    <div v-if="contact" class="page">
+    <h4>Hiệu chỉnh Liên hệ</h4>
+    <ContactForm 
+    :contact="contact"
+    @submit:contact="updateContact"
+    @delete:contact="deleteContact"/>
+    <p>{{ message}}</p>
+    </div>
+</template>
+
+<script>
+import ContactFrom from "@/components/ContactFrom.vue";
+import ContactService from "@/services/contact.service";
+export default {
+    components: {
+        ContactForm,
+    },
+    props: {
+        id: {type:String, required: true },
+    },
+    data() {
+        return {
+            contact: null,
+            message: "",
+
+        };
+    },
+    method: {
+        async getContact(id) {
+            try {
+                this.contact = await ContactService.get(id);
+          } catch (error) {
+            console.log(error);
+        
+          // Chuyển sang trang NotFound đồng thời giữ cho URL không đói
+          this.$router.push({
+            name: "notfound",
+            params: {
+                pathMatch: this.$route.path.split("/").slice(1)
+            },
+            query: this.$route.query,
+            hash: this.$route.hash,
+          });
+        }
+    },
+    async updateContact(data){
+        try {
+            await ContactService.update(this.contact._id, data);
+            this.mesage = "Liên hệ được câp nhật thành công.";
+        } catch (error) {
+            console.log(error);
+        }
+
+    },
+    async deleteContact() {
+        if (confirm("Bạn muốn xóa liên hệ này")) {
+            try {
+                await ContactService.delete(this.contact._id);
+                this.$router.push({ name: "contactbook" });
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    },
+},
+created() {
+    this.getContact(this.id);
+    this.message ="";
+},
+};
+</script>
